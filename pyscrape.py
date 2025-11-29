bbef8758fc232290b44843d2fad9c9fca68a1142
Cereal_Dev <ajedavid1@gmail.com>
2025-11-29 16:44:05 +0000

    base starting code for finding all the tables in the website i ran into an issue cus most of the stock sites where dynamic so i fixed that by using selenium, and i was able to access the shadow roots through using pyshadow

--- pyscrape.py
+++ pyscrape.py
@@ -1,39 +1,33 @@
-##Starting test code to just scrape the <table> elements in any page and parse it 
-import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin
-
-# web = requests.get('https://github.com/the-cerealdev')
-# web = requests.get('https://www.londonstockexchange.com/news-article/market-news/form-8-3-just-group-plc/17344088') #this happens to be a dynamic website
-url='https://www.w3schools.com/html/html_tables.asp'
-web = requests.get('https://www.w3schools.com/html/html_tables.asp')
-soup = BeautifulSoup(web.content,'html.parser')
-
-tables = soup.find('table')
-print(tables)
-if tables:
-    print(tables)
-else:
-    print('no tables found')
-print(web.status_code)
-
-with open('output.html', 'w', encoding='utf-8') as f:
-    f.write('<html><head><meta charset="utf-8"></head><body>')
-    f.write('<style>')
-    for style_tag in soup.find_all('style'):
-        f.write(style_tag.get_text())
-    for link in soup.find_all('link', rel='stylesheet'):
-        css_link = urljoin(url,link['href'])
-       
-        css_response = requests.get(css_link)
-        f.write(str(css_response))
-    f.write('</style>')
-    for element in tables:
-        
-        f.write(str(element))
-
-    f.write('</body></html>')
 
 
-f.close()
-# print(soup.prettify())
+from selenium import webdriver
+from pyshadow.main import Shadow
+from webdriver_manager.chrome import ChromeDriverManager
+import time 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+
+
+url = "https://www.londonstockexchange.com/news-article/market-news/form-8-3-just-group-plc/17344088"
+
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+driver.get(url)
+time.sleep(5)
+
+
+tables = driver.find_elements(By.TAG_NAME, 'table')
+if not tables:
+    '''
+
+    if the normal search for <table> elements doesnt bring anything
+    it searches the shadow roots
+    
+    '''
+    shadow = Shadow(driver)
+    tables = shadow.find_elements('table')
+
+for table in tables:
+    print(table.text)
